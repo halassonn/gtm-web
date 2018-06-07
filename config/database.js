80342faf6dde42ea6f955ee30347a9e00dc80709
halassonn <gtm24halas@gmail.com>
2018-06-07 22:46:18 +0700

    commit db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
     //database : 'mongodb://localhost:27017/dioriganda',
-    database : 'mongodb://localhost:27017/test3'
+    //database : 'mongodb://localhost:27017/test3'
+
+    database : 'mongodb://halasson24:gtm241186@ds151970.mlab.com:51970/test3'
 }
